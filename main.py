a634233235c18ec886454a71be10ccfacf4843e0
yerim11-asd <yealim0401@gmail.com>
2024-12-08 23:46:48 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#from dotenv import load_dotenv
+#load_dotenv()
+
+from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
+
+import streamlit as st
+
+st.tilte("인공지능 시인")
+subject = st.text_input('시의 주제를 입력해주세요')
+st.write('시의 주제:'+ subject)
+
+if st.button('시 작성'):
+    with st.spinner('시 작성중...'):
+        result = chat_model.invoke(subject + '에 대한 시를 써줘')
+        st.write(result.content)
